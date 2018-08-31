29b0724259d736b4d1959cac195bb92c495b99dd
daultonryan <daultonoryan@gmail.com>
2018-08-30 23:11:35 -0400

    added a new branch to work with a fileset that doesnt involve images because of playground space and because of focus on RPi-zero updated user interface to be clean but minimal

--- greenthumb/views.py
+++ greenthumb/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, request
 from .models import Sensors
 from .sterilizers import SensorSerializer
+
 
 # Create your views here.
 
 class SensorViewSet(viewsets.ModelViewSet):
     queryset = Sensors.objects.all()
     serializer_class = SensorSerializer
+
 
 
 
